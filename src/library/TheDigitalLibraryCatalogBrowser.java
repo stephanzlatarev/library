6d4f4af24ae716f023874ce96061c32d82fed82a
Stephan Zlatarev <stephan.zlatarev@sap.com>
2012-11-15 01:43:11 +0200

    LIB-201: Library offers a catalog browser to "The Digital Library"
    Refactored catalog browser for the digital library

--- src/library/TheDigitalLibraryCatalogBrowser.java
+++ src/library/TheDigitalLibraryCatalogBrowser.java
@@ -3,34 +3,75 @@ package library;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import tdl.SearchResults;
 import tdl.SearchSession;
 import tdl.TheDigitalLibrary;
 
 public class TheDigitalLibraryCatalogBrowser {
 
-	public TheDigitalLibraryCatalogBrowser() {
-		TheDigitalLibrary.getLibrary().connect();
+	private TheDigitalLibrary library;
+	private SearchSession session;
+
+	public TheDigitalLibraryCatalogBrowser(TheDigitalLibrary library) {
+		this.library = library;
+		this.library.connect();
 	}
 
 	public Collection<BookTitle> search(String keyword) {
-		SearchSession session = TheDigitalLibraryUtils.openSearch(keyword);
-
 		try {
 			Collection<BookTitle> result = new ArrayList<BookTitle>();
-			Collection<String> headers = TheDigitalLibraryUtils.getFirstPageOfHeaders(session, keyword);
+			Collection<String> headers = getFirstPageOfHeaders(keyword);
 	
 			while (!headers.isEmpty()) {
 				for (String header: headers) {
-					result.add(TheDigitalLibraryUtils.constructBookTitle(header));
+					result.add(constructBookTitle(header));
 				}
 	
-				headers = TheDigitalLibraryUtils.fetchNextPageOfHeaders(session);
+				headers = fetchNextPageOfHeaders();
 			}
 
 			return result;
 		} finally {
-			TheDigitalLibraryUtils.closeSearch(session);
+			closeSearch();
 		}
+	}
+
+	private final Collection<String> getFirstPageOfHeaders(String keyword) {
+		session = library.openSearchSession();
+
+		SearchResults search = library.startSearch(session, keyword, 20);
+
+		return readPageOfHeaders(search);
+	}
+
+	private final Collection<String> fetchNextPageOfHeaders() {
+		SearchResults search = library.fetchNextPage(session);
+
+		return readPageOfHeaders(search);
+	}
+
+	private final Collection<String> readPageOfHeaders(SearchResults search) {
+		ArrayList<String> page = new ArrayList<String>();
+
+		while (search.hasNextBookTitle()) {
+			page.add(search.getBookTitle());
+		}
+
+		return page;
+	}
+
+	private final void closeSearch() {
+		if (session != null) {
+			library.closeSearchSession(session);
+		}
+	}
+
+	private final BookTitle constructBookTitle(String bookHeading) {
+		int index = bookHeading.lastIndexOf(" by ");
+		String title = bookHeading.substring(0, index);
+		String author = bookHeading.substring(index + 4);
+
+		return new BookTitle(author, title);
 	}
 
 }
